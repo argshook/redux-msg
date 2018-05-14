ea008ea45bf18a8c274e2e1d08f7580e0ae52262
argshook <argshook@gmail.com>
2018-05-14 22:13:30 +0300

    chore(create-selectors): remove
    it's a leftover, forgot to remove this file
    it was renamed to createSelector (singular) since 2.0.0

--- src/create-selectors.js
+++
@@ -1,8 +0,0 @@
-// createSelector : NAME => MODEL => ({ MODEL.key: (state -> value) })
-export default name => model =>
-  Object
-    .keys(model)
-    .reduce((selectors, key) => {
-      selectors[key] = state => state[name][key];
-      return selectors;
-    }, {});
